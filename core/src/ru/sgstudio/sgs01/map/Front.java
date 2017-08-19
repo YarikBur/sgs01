31ecbd53cb62e16e51ef1dd4f5cd400715df86e1
Alexander Smirnov <smirnov48@gmail.com>
2017-08-19 15:24:23 +0300

    fix render terrain

--- core/src/ru/sgstudio/sgs01/map/Front.java
+++ core/src/ru/sgstudio/sgs01/map/Front.java
@@ -37,11 +37,14 @@ public class Front {
 	
 	public void render(SpriteBatch batch){
 		System.out.print('\n');
-		for(int y=0;y<map[0].length;y++){
-			for(int x=0;x<map[0][0].length;x++){
-				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), x*16, y*16);
-				System.out.format("%2d", map[zPlayer][y][x]);
-				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), 5*16, 0*16);
+		for (int y = map[0].length - 1; y >= 0; y--) {
+			for (int x = 0; x < map[0][0].length; x++) {
+				int dispX = x * 16;
+				int dispY = (map[0].length - y) * 16;
+
+				batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), dispX, dispY);
+				// System.out.format("%2d", map[zPlayer][y][x]);
+				// batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), 5 * 16, 0 * 16);
 			}
 			System.out.print('\n');
 		}
