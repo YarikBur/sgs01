ff98937456b103362ca0dd008f699d8ed9b88213
YarikBur <yarikbur@Mail.ru>
2017-08-19 15:27:49 +0300

    fix

--- core/src/ru/sgstudio/sgs01/map/Front.java
+++ core/src/ru/sgstudio/sgs01/map/Front.java
@@ -36,7 +36,6 @@ public class Front {
 	
 	public void render(SpriteBatch batch){
 		System.out.print('\n');
-<<<<<<< Updated upstream
 		for (int y = map[0].length - 1; y >= 0; y--) {
 			for (int x = 0; x < map[0][0].length; x++) {
 				int dispX = x * 16;
@@ -45,13 +44,6 @@ public class Front {
 				batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), dispX, dispY);
 				// System.out.format("%2d", map[zPlayer][y][x]);
 				// batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), 5 * 16, 0 * 16);
-=======
-		for(int y=map[0].length-1;y>=0;y--){
-			for(int x=0;x<map[0][0].length;x++){
-				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), x*16, y*16);
-				System.out.format("%2d", map[zPlayer][y][x]);
-				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), 5*16, 0*16);
->>>>>>> Stashed changes
 			}
 			System.out.print('\n');
 		}
