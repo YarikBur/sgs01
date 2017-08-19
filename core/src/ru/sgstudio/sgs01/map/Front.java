ec6c455cddcbbf7cbf7e9811ee1bc0e579d1f5a7
YarikBur <yarikbur@Mail.ru>
2017-08-19 15:25:51 +0300

    Auto stash before merge of "master" and "origin/master"

--- core/src/ru/sgstudio/sgs01/map/Front.java
+++ core/src/ru/sgstudio/sgs01/map/Front.java
@@ -2,7 +2,6 @@ package ru.sgstudio.sgs01.map;
 
 import java.util.Map;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
@@ -37,6 +36,7 @@ public class Front {
 	
 	public void render(SpriteBatch batch){
 		System.out.print('\n');
+<<<<<<< Updated upstream
 		for (int y = map[0].length - 1; y >= 0; y--) {
 			for (int x = 0; x < map[0][0].length; x++) {
 				int dispX = x * 16;
@@ -45,6 +45,13 @@ public class Front {
 				batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), dispX, dispY);
 				// System.out.format("%2d", map[zPlayer][y][x]);
 				// batch.draw(atlasTiles.get("tiles0_" + map[zPlayer][y][x]), 5 * 16, 0 * 16);
+=======
+		for(int y=map[0].length-1;y>=0;y--){
+			for(int x=0;x<map[0][0].length;x++){
+				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), x*16, y*16);
+				System.out.format("%2d", map[zPlayer][y][x]);
+				batch.draw(atlasTiles.get("tiles0_"+map[zPlayer][y][x]), 5*16, 0*16);
+>>>>>>> Stashed changes
 			}
 			System.out.print('\n');
 		}
