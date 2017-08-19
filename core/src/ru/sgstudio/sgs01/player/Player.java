1cd1de64926334da1b648a6bdd21aae6e3ed7a36
Maxim Gorovets <gorovec3@gmail.com>
2017-08-19 16:01:43 +0300

    Created class player and  a player batch
    Check class player

--- core/src/ru/sgstudio/sgs01/player/Player.java
+++ core/src/ru/sgstudio/sgs01/player/Player.java
@@ -1,7 +1,12 @@
 package ru.sgstudio.sgs01.player;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Player {
 	private static int zPlayer, yPlayer, xPlayer;
+	private static Sprite spritePlayer;
 	
 	public int getZPlayer(){ return zPlayer; }
 	public int getYPlayer(){ return yPlayer; }
@@ -10,4 +15,15 @@ public class Player {
 	public void setZPlayer(int zPlayer){ Player.zPlayer = zPlayer; }
 	public void setYPlayer(int yPlayer){ Player.yPlayer = yPlayer; }
 	public void setXPlayer(int xPlayer){ Player.xPlayer = xPlayer; }
+	
+	public void setPlayer(Sprite sprite) {
+		Player.spritePlayer = sprite;
+	}
+	public void draw(Batch spriteBatch) {
+		update(Gdx.graphics.getDeltaTime());
+		spritePlayer.draw(spriteBatch);
+	}
+	public void update(float deltaTime) {
+		
+	}
 }
