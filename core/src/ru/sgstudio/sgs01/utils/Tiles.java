ef976a05894a08e7cd043c4dff827d3115229373
YarikBur <yarikbur@Mail.ru>
2017-08-20 17:25:13 +0300

    com

--- core/src/ru/sgstudio/sgs01/utils/Tiles.java
+++ core/src/ru/sgstudio/sgs01/utils/Tiles.java
@@ -6,12 +6,28 @@ import java.util.Map;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
+/**
+ * Используется для разрезания картинки
+ * @author Yarik
+ * @version 1.0
+ *
+ */
+
 public class Tiles {
+	
+	
 	private static Texture texture = null;
 	private static int Lines = 0;
 	private static int Columns = 0;
 	
 	private static Map<String, TextureRegion> textureRegions = null;
+	
+	/**
+	 * Позволяет создать атлас(textureRegions)
+	 * @param tiles - Путь до картинки
+	 * @param lines - Кол-во линий
+	 * @param columns - Кол-во столбцов
+	 */
 	
 	public void createAtlas(String tiles, int lines, int columns){
 		texture = new Texture(tiles);
@@ -27,6 +43,11 @@ public class Tiles {
 		for(int y=0;y<Lines;y++) for(int x=0;x<Columns;x++) textureRegions.put("tiles"+y+"_"+x, tmp[y][x]);
 		Lines=0; Columns=0; texture=null;
 	}
+	
+	/**
+	 * Возвращает разрезанный атлас
+	 * @return textureRegions
+	 */
 	
 	public Map<String, TextureRegion> getTextureRegion(){ return textureRegions; }
 }
